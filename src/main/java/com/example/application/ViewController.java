005755c1b610b4f3132ee10fd7be1af0ddb2f45d
Cristyane Tamioso <christiehespanha@gmail.com>
2022-09-02 10:11:19 -0300

    handling events using controller

--- src/main/java/com/example/application/ViewController.java
+++ src/main/java/com/example/application/ViewController.java
@@ -3,12 +3,12 @@ package com.example.application;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class ViewController {
     @FXML
     private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        welcomeText.setText("Yay! You clicked me!");
     }
 }
