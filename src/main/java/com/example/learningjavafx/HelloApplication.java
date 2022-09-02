8d0a4337a1006cedd072aa14f4e47a32a2bfe5fe
Cristyane Tamioso <christiehespanha@gmail.com>
2022-09-02 09:20:57 -0300

    reset project

--- src/main/java/com/example/learningjavafx/HelloApplication.java
+++ src/main/java/com/example/learningjavafx/HelloApplication.java
@@ -10,17 +10,9 @@ import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
-    /* public void start(Stage stage) throws IOException {
+    public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
-    } */
-
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Teste.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
